80c784aea26a6432f5fd18313f678652036bd180
Juan José Nolasco Meza <juanjosenolascomeza@192.168.1.2>
2025-08-07 19:30:35 -0600

    auth functionality

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <div className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </div>
+  );
 };
 
 export default AllUsers;
